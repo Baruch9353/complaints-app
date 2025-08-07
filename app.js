a4ad142c118ab1a2d87b35a4d84f86fff028aa6d
baruch9353 <baruchminzberg@gmail.com>
2025-08-07 12:14:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import { config } from 'dotenv';
+
+config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
